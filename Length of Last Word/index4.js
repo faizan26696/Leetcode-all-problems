ad5470612b5f0c72e23c791962589995bcf959b1
Mohammad Faizan <111674496+faizan26696@users.noreply.github.com>
2023-04-02 15:41:23 +0530

    Length of Last Word

---
+++ Length of Last Word/index4.js
@@ -0,0 +1,10 @@
+// Q)Length of Last Word
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+  if (s.length == 0) return 0;
+  let arr = s.trim().split(" ");
+  return arr[arr.length - 1].length;
+};
